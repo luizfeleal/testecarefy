87f82f9d7d81b2006b50446a7106c3908831916e
Luiz Felipe <felipelearaujo@gmail.com>
2023-02-07 14:33:12 -0300

    Estrutura

--- FrontCarefy/resources/views/login.blade.php
+++ FrontCarefy/resources/views/login.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+    <script src="https://kit.fontawesome.com/868b3a4d54.js" crossorigin="anonymous"></script>
     <title>Login</title>
 </head>
 <body>
@@ -14,14 +15,27 @@
     <div class="container">
         <div class="content">
             <h1>Login</h1>
-            <input type="email" name="email" id="email"  class="input-text" placeholder="E-Mail">
-            <input type="password" name="password" class="input-text" id="password" placeholder="Senha">
-            <button type="button" class="button-primary">Login</button>
-            <hr/>
+            <div class='input-container'>
+                <label for="email"><img src="{{ asset('img/icons/envelope.png') }}" alt="Envelope icon" width="18px" height="18px"></label>
+                <input type="email" name="email" id="email-login"  class="input-text" placeholder="E-Mail">
+            </div>
+            <div class='input-container'>
+            <label for="password"><img src="{{ asset('img/icons/lock.png') }}" alt="Lock icon" width="18px" height="18px"></label>
+                <input type="password" name="password" class="input-text" id="password" placeholder="Senha">
+            </div>
+            <div class="div-button-login">
+                <button type="button" class="button-primary">Login</button>
+            </div>
+            <hr>
             <p>Ou</p>
             <h2>Conecte-se com</h2>
-            <button type="button" class="button-with-icon-secundary">GitHub</button>
+            <div class="div-button-login-github">
+                <button type="button" class="button-with-icon-secundary"><img src="{{ asset('img/icons/github-icon-white.png') }}" alt="Lock icon" width="40px" height="30px"> GitHub</button>
+            </div>
         </div>
     </div>
+
+    <script src="{{ asset('js/Validations.js') }}"></script>
+    <script src="{{ asset('js/Events.js') }}"></script>
 </body>
 </html>
