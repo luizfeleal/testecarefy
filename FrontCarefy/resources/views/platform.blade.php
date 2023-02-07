a13d22edec60cc4e04d88c5aecc117d9affabae2
Luiz Felipe <felipelearaujo@gmail.com>
2023-02-07 14:35:09 -0300

    Criação de estrutura da área logada

---
+++ FrontCarefy/resources/views/platform.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+    <title>Plataforma</title>
+</head>
+<body>
+    <nav>
+        <img src="{{ asset('img/logo/logoCarefy.png') }}" alt="Logo Carefy">
+    </nav>
+
+    <div class="container">
+        <div class="content-platform">
+            <h1>Plataforma</h1>
+
+            <table class="table-fill">
+                <thead>
+                    <tr>
+                    <th class="text-left">Nome</th>
+                    <th class="text-left">Editar</th>
+                    <th class="text-left">Excluir</th>
+                    </tr>
+                </thead>
+                <tbody class="table-hover">
+                    <tr>
+                        <td class="text-left">João</td>
+                        <td class="text-left">Editar</td>
+                        <td class="text-left">Excluir</td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">Miguel</td>
+                        <td class="text-left">Editar</td>
+                        <td class="text-left">Excluir</td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">Felipe</td>
+                        <td class="text-left">Editar</td>
+                        <td class="text-left">Excluir</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
