1658540d7936d8d0c3e8115d0ffd1949475875b0
Luiz Felipe <felipelearaujo@gmail.com>
2023-02-09 13:51:30 -0300

    Criação de rotas para autenticação com github e autenticação simples

--- FrontCarefy/routes/web.php
+++ FrontCarefy/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use App\Models\UserAuthenticate;
+use App\Http\Controllers\UserAuthenticateController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +25,38 @@ Route::get('/login', function () {
     return view('login');
 });
 Route::get('/platform', function () {
-    return view('platform');
+    if(session()->has('usuario_id')){
+
+        return view('platform');
+
+   }else{
+
+    return view('login');
+   }
+})->name('platform.page');
+
+
+
+
+Route::get('/auth/redirect', function () {
+     return Socialite::driver('github')->redirect();
+});
+Route::get('/auth/login', [UserAuthenticateController::class, 'auth']);
+Route::get('/auth/callback', function () {
+     $githubUser = Socialite::driver('github')->user();
+
+     $githubUserData = array(
+        "name"=> $githubUser->name,
+        "email"=>$githubUser->email,
+        "github_id"=> $githubUser->id,
+        "github_token"=> $githubUser->token,
+        "github_refresh_token" => $githubUser->refreshToken,
+     );
+
+     $user = User::createUser($githubUserData);
+
+     session()->put('usuario_id', $user->id);
+
+     return redirect()->route('platform.page');
+
 });
