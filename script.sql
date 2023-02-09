e3dbd3089cb39811cc628a42d0aff171dfd714e6
Luiz Felipe Leal de Araujo <felipelearaujo@gmail.com>
2023-02-09 18:33:34 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,25 +1,72 @@
-DROP DATABASE IF EXISTS testeCarefy;
+DROP DATABASE IF EXISTS testecarefy;
 
-CREATE DATABASE testeCarefy;
+CREATE DATABASE testecarefy;
 
-USE testeCarefy;
+USE testecarefy;
 
-CREATE TABLE clientes(
-    id int(11) NOT NULL AUTO_INCREMENT,
-    nome varchar(50),
-    data_criacao timestamp default current_timestamp(),
-    data_alteracao timestamp default current_timestamp() on UPDATE current_timestamp(),
-    PRIMARY KEY(id)
-);
+--
+-- Banco de dados: `testecarefy`
 
-CREATE TABLE usuario(
-    id int(11) NOT NULL AUTO_INCREMENT,
-    nome varchar(50),
-    email varchar(50),
-    senha varchar(50),
-    data_criacao timestamp default current_timestamp(),
-    data_alteracao timestamp default current_timestamp() on UPDATE current_timestamp(),
-    data_ultimo_acesso timestamp default current_timestamp() on UPDATE current_timestamp(),
-    PRIMARY KEY(id),
-    UNIQUE KEY(email)
-);
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL ,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(16) NOT NULL,
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data_alteracao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `github_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `github_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `github_refresh_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
